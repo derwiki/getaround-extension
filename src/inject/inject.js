3ab215944c0ef3e10061bdde32b5b2164c556748
Adam Derewecki <derewecki@gmail.com>
2016-11-27 12:41:44 -0800

    Poll for Ajax to complete

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,20 +1,30 @@
+var earnings = function() {
+  if ($('.past-activity .rental-price-info').length === 0) {
+    console.log('.');
+    return setTimeout(earnings, 500);
+  }
+
+  var arr = $(".rental-price-info").map(function(idx, ele) {
+    var price = $(ele).text().split("\n")[1].trim();
+    var decimal = parseFloat(price.substring(1));
+    return decimal;
+  });
+  var n = arr.length;
+  var sum = 0;
+  while (n--) { sum += arr[n] }
+
+  var $carInfoBox = $('.card-text-details:first');
+  $carInfoBox.append('<div>Current: $' + sum + '</div>');
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
+
       console.log("Loading GetAround Buddy...");
-
-      var arr = $(".rental-price-info").map(function(idx, ele) {
-        var price = $(ele).text().split("\n")[1].trim();
-        var decimal = parseFloat(price.substring(1));
-        return decimal;
-      });
-      var n = arr.length;
-      var sum = 0;
-      while (n--) { sum += arr[n] }
-
-      var t = $('.card-text-details:first').text();
-      $('.card-text-details:first').text(t + ' (current $' + sum + ')');
+      earnings();
     }
-	}, 12000);
+
+	}, 10);
 });
